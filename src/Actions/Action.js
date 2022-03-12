3644ef6e8f9c622b09c0c3887cee25734fa14986
vitorcdn11 <vitorcdn11@gmail.com>
2022-03-12 18:07:23 -0300

    Redux: Adding the Edit modal Action

--- src/Actions/Action.js
+++ src/Actions/Action.js
@@ -1,8 +1,12 @@
 const modalAction = (value) => { return { type: value ? "SHOW_MODAL" : "HIDDE_MODAL" }}
+const modalEditAction = (value, content) => {
+    return { type: value ? "SHOW_EDIT_MODAL" : "HIDDE_EDIT_MODAL", payload: content}
+}
 
 const listAction = (type, value) => { return { type, payload: value}}
 
 export {
     modalAction,
+    modalEditAction,
     listAction
 }
